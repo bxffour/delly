93dba2403a2b6f7ef71c76be7ed776bf519a1e93
jerry.buaba <jerry.buaba@mpharma.coma>
2023-09-27 23:20:46 +0100

    this was so tiring

--- main.go
+++ main.go
@@ -2,33 +2,195 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
-	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"text/tabwriter"
 
 	"github.com/dustin/go-humanize"
 	"github.com/urfave/cli/v2"
 )
 
-type metadata struct {
-	dMeta dirMap
-	fMeta fileMap
-	total int64
+type Metadata struct {
+	Dirs  map[string]*DirMeta
+	Files map[string]int64
+	Total int64
 }
 
-type (
-	dirMap  map[string]dirMeta
-	fileMap map[string]int64
-)
+type DirMeta struct {
+	Size    int64
+	Deleted int64
+}
 
-type dirMeta struct {
-	size         int64
-	bytesDeleted int64
+type Reporter interface {
+	Report() error
+}
+
+type FileReporter struct {
+	Files map[string]int64
+	Total int64
+}
+
+func (f *FileReporter) Report() error {
+	w := tabwriter.NewWriter(os.Stdout, 12, 1, 3, ' ', 0)
+
+	fmt.Fprint(w, "FILE\tSIZE\n")
+	fmt.Fprint(w, "----\t----\n")
+
+	for path, size := range f.Files {
+		fmt.Fprintf(w, "%s\t%s\n", path, humanize.Bytes(uint64(size)))
+	}
+
+	fmt.Fprint(w, "----\t----\n")
+	fmt.Fprintf(w, "TOTAL\t%s\n", humanize.Bytes(uint64(f.Total)))
+
+	if err := w.Flush(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type DirReporter struct {
+	Dirs map[string]*DirMeta
+}
+
+func (d *DirReporter) Report() error {
+	w := tabwriter.NewWriter(os.Stdout, 12, 1, 3, ' ', 0)
+
+	fmt.Fprint(w, "DIRECTORY\tOLDSIZE\tNEWSIZE\tBYTES SAVED\n")
+	fmt.Fprint(w, "---------\t-------\t-------\t-----------\n")
+
+	for dir, meta := range d.Dirs {
+		if meta.Deleted > 0 {
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\n",
+				dir,
+				humanize.Bytes(uint64(meta.Size)),
+				humanize.Bytes(uint64(meta.Size-meta.Deleted)),
+				humanize.Bytes(uint64(meta.Deleted)),
+			)
+		}
+	}
+
+	if err := w.Flush(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func WalkDirs(root string, exts []string) chan Metadata {
+	outChan := make(chan Metadata)
+
+	go func() {
+		defer close(outChan)
+
+		meta := Metadata{
+			Dirs:  make(map[string]*DirMeta),
+			Files: make(map[string]int64),
+		}
+
+		err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+			dir := filepath.Dir(path)
+
+			if !info.IsDir() {
+				ext := strings.ToLower(strings.TrimPrefix(filepath.Ext(info.Name()), "."))
+				if matchExt(ext, exts) {
+					meta.Files[path] = info.Size()
+					meta.Total += info.Size()
+				}
+			}
+
+			dm, ok := meta.Dirs[dir]
+			if !ok {
+				dm = &DirMeta{}
+			}
+			dm.Size += info.Size()
+			meta.Dirs[dir] = dm
+
+			return nil
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		outChan <- meta
+	}()
+
+	return outChan
+}
+
+func deleteFiles(meta Metadata) chan Metadata {
+	outChan := make(chan Metadata)
+
+	go func() {
+		defer close(outChan)
+
+		var wg sync.WaitGroup
+		var filesToDelete []string
+
+		for path := range meta.Files {
+			filesToDelete = append(filesToDelete, path)
+		}
+
+		if len(filesToDelete) == 0 {
+			fmt.Println("Nothing to delete")
+			outChan <- meta
+			return
+		}
+
+		var mu sync.Mutex
+		var totalDeleted int64
+
+		for _, path := range filesToDelete {
+			wg.Add(1)
+
+			go func(path string) {
+				defer wg.Done()
+
+				err := os.Remove(path)
+				if err != nil {
+					log.Printf("Error deleting %s: %v", path, err)
+					return
+				}
+
+				dir := filepath.Dir(path)
+
+				mu.Lock()
+				dm, ok := meta.Dirs[dir]
+				if ok {
+					dm.Deleted += meta.Files[path]
+					meta.Dirs[dir] = dm
+				}
+
+				delete(meta.Files, path)
+
+				for dir != "." && dir != "/" {
+					dm, ok := meta.Dirs[dir]
+					if ok {
+						dm.Deleted += meta.Files[path]
+						meta.Dirs[dir] = dm
+					}
+					dir = filepath.Dir(dir)
+				}
+				mu.Unlock()
+
+				totalDeleted += meta.Files[path]
+			}(path)
+		}
+
+		wg.Wait()
+
+		meta.Total -= totalDeleted
+
+		outChan <- meta
+	}()
+
+	return outChan
 }
 
 func main() {
@@ -43,197 +205,76 @@ func main() {
 				Required: true,
 			},
 		},
-		Before: func(ctx *cli.Context) error {
-			args := ctx.Args()
-			if args.Len() != 1 {
-				return errors.New("error invalid args: exactly one argument must be provided")
-			}
-			return nil
-		},
-		Action: func(ctx *cli.Context) error {
-			exts := ctx.StringSlice("ext")
-			rootDir := ctx.Args().Get(0)
-
-			meta, err := collectDirMetadata(rootDir, exts)
-			if err != nil {
-				return err
+		Action: func(c *cli.Context) error {
+			if c.NArg() != 1 {
+				return fmt.Errorf("Usage: delly [global options] <directory>")
 			}
 
-			if meta.total == 0 {
-				fmt.Println("There is nothing to delete. Exiting...")
+			rootDir := c.Args().Get(0)
+			exts := c.StringSlice("ext")
+
+			metaChan := WalkDirs(rootDir, exts)
+			meta := <-metaChan
+
+			if meta.Total == 0 {
+				fmt.Println("Nothing to delete")
 				return nil
 			}
 
-			if err := meta.reportFileMetadata(); err != nil {
-				return err
+			reporter := &FileReporter{meta.Files, meta.Total}
+			if err := reporter.Report(); err != nil {
+				log.Fatal(err)
 			}
 
-			confirm := askForConfirmation("do you want to go ahead with deleting these files?")
-
+			confirm := askConfirm("Confirm delete? [y/n]: ")
 			if !confirm {
-				fmt.Println("exiting...")
+				fmt.Println("Exiting...")
 				return nil
 			}
 
-			meta, err = deleteFilesByExtension(meta)
-			if err != nil {
-				return err
-			}
+			delChan := deleteFiles(meta)
+			meta = <-delChan
 
-			if err := meta.reportDirMetadata(); err != nil {
-				return err
+			dirReporter := &DirReporter{meta.Dirs}
+			if err := dirReporter.Report(); err != nil {
+				log.Fatal(err)
 			}
 
 			return nil
 		},
 	}
 
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func (d dirMap) report() error {
-	w := tabwriter.NewWriter(os.Stdout, 12, 1, 3, ' ', 0)
-	fmt.Fprint(w, "DIRECTORY\tOLDSIZE\tNEWSIZE\tBYTES SAVED\n")
-	fmt.Fprint(w, "---------\t-------\t-------\t-----------\n")
-	for k, v := range d {
-		if v.bytesDeleted != 0 {
-			size := humanize.Bytes(uint64(v.size))
-			newsz := humanize.Bytes(uint64(v.size - v.bytesDeleted))
-			bytesSaved := humanize.Bytes(uint64(v.bytesDeleted))
-
-			fmt.Fprintf(w, "%s\t%s\t%s\t%s\n",
-				k,
-				size,
-				newsz,
-				bytesSaved,
-			)
-		}
-	}
-	if err := w.Flush(); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Print("\n")
-	return nil
-}
-
-func (m metadata) reportFileMetadata() error {
-	if m.total == 0 {
-		return nil
-	}
-
-	return m.fMeta.report(m.total)
-}
-
-func (m metadata) reportDirMetadata() error {
-	return m.dMeta.report()
-}
-
-func (f fileMap) report(total int64) error {
-	w := tabwriter.NewWriter(os.Stdout, 12, 1, 3, ' ', 0)
-	fmt.Fprint(w, "FILE\tSIZE\n")
-	fmt.Fprint(w, "----\t----\n")
-	for k, v := range f {
-		fmt.Fprintf(w, "%s\t%s\n", k, humanize.Bytes(uint64(v)))
-	}
-
-	fmt.Fprint(w, "----\t----\n")
-	fmt.Fprintf(w, "TOTAL\t%s\n\n", humanize.Bytes(uint64(total)))
-
-	if err := w.Flush(); err != nil {
-		log.Fatal(err)
-	}
-
-	return nil
-}
-
-func deleteFilesByExtension(meta metadata) (metadata, error) {
-	for path, size := range meta.fMeta {
-		dir := filepath.Dir(path)
-		sz, ok := meta.dMeta[dir]
-		if ok {
-			err := os.Remove(path)
-			if err != nil {
-				return metadata{}, err
-			}
-			sz.bytesDeleted += size
-			meta.dMeta[dir] = sz
-		}
-	}
-
-	return meta, nil
-}
-
-func collectDirMetadata(rootdir string, exts []string) (metadata, error) {
-	dmap := make(dirMap)
-	fmap := make(fileMap)
-	var total int64
-
-	err := filepath.Walk(rootdir, func(path string, info fs.FileInfo, err error) error {
-		if info.IsDir() {
-			var d dirMeta
-			dmap[path] = d
-		}
-
-		if !info.IsDir() {
-			if matchExt(info.Name(), exts) {
-				size := info.Size()
-				fmap[path] = size
-				total += size
-			}
-
-			dir := filepath.Dir(path)
-			sz, ok := dmap[dir]
-			if ok {
-				sz.size += info.Size()
-				dmap[dir] = sz
-			}
-		}
-
-		return nil
-	})
+	err := app.Run(os.Args)
 	if err != nil {
-		return metadata{}, err
+		log.Fatal(err)
 	}
-
-	return metadata{
-		dMeta: dmap,
-		fMeta: fmap,
-		total: total,
-	}, nil
 }
 
-func matchExt(file string, ext []string) bool {
-	for _, e := range ext {
-		if strings.TrimLeft(filepath.Ext(file), ".") == e {
-			return true
-		}
-	}
-	return false
-}
-
-func askForConfirmation(s string) bool {
+func askConfirm(prompt string) bool {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		fmt.Printf("%s [y/n]: ", s)
+		fmt.Printf("%s", prompt)
 
-		response, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSpace(text)
 
-		response = strings.ToLower(strings.TrimSpace(response))
-
-		fmt.Print("\n")
-
-		switch response {
+		switch strings.ToLower(text) {
 		case "y", "yes":
 			return true
+
 		case "n", "no":
 			return false
 		}
 	}
+}
+
+func matchExt(ext string, exts []string) bool {
+	ext = strings.ToLower(ext)
+	for _, validExt := range exts {
+		if ext == validExt {
+			return true
+		}
+	}
+	return false
 }
